7ffba0ea1a774c3372705d9cfe3398d8191a4812
sheezu900 <shahzamanlive2@gmail.com>
2023-10-10 15:17:07 +0500

    update option file

--- options.rb
+++ options.rb
@@ -1,6 +1,10 @@
 require_relative 'app'
 
 class Options
+  def initialize(app)
+    @app = app
+  end
+
   def list_options
     puts "\n"
     puts 'Enter number to pick an option:'
@@ -14,18 +18,23 @@ class Options
   end
 
   def option_handle(option)
-    app = App.new
-    actions = {
-      1 => -> { app.list_books },
-      2 => -> { app.list_people },
-      3 => -> { app.create_person },
-      4 => -> { app.create_book },
-      5 => -> { app.create_rental },
-      6 => -> { app.list_rentals }
-    }
     case option
-    when 1, 2, 3, 4, 5, 6, 7
-      actions[option].call
-    end
-  end
+    when 1
+      @app.list_books
+    when 2
+      @app.list_people
+    when 3
+      @app.create_person
+    when 4
+      @app.create_book
+    when 5
+      @app.create_rental
+    when 6
+      @app.list_rentals
+    when 7
+      exit
+    else
+      puts 'Invalid option'
+    end
+  end
 end
