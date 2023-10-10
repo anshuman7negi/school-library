740ee522e712f708d355fde70fb7ffaa7d4db171
sheezu900 <shahzamanlive2@gmail.com>
2023-10-10 15:04:42 +0500

    refector code

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require_relative 'options'
 
 def main
-  puts 'Welcome to School library App'  
+  puts 'Welcome to School library App'
 
   list = Options.new
   loop do
     list.list_options
     option = gets.chomp.to_i
+    break if option == 7
+
     list.option_handle(option)
   end
 end
